3b0783d0d5a0bf538c8706d5a498ad202ea9923e
Varun <vvijay@uvm.edu>
2025-06-09 23:52:13 -0400

    added map hopefully

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let map;
 
 function initMap() {
     map == new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 44.4779, lng: -73.1965 },
-        zoom: 12
+        center: { lat: 44.4781, lng: -73.1965 },
+        zoom: 16,
+        mapTypeId: 'satellite'
     });
+
+    map.addListener("click", (event) => {
+        new google.maps.Marker({
+          position: event.latLng,
+          map: map,
+        });
+      });
 }
