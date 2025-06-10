9298e05f1aae9c2803404d16e56a7e9432e49550
Varun <vvijay@uvm.edu>
2025-06-10 08:57:25 -0400

    changed map center point

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
     map == new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 44.4781, lng: -73.1965 },
+        center: { lat: 44.4781, lng: -73.1975 },
         zoom: 16,
         mapTypeId: 'satellite'
     });
@@ -12,5 +12,11 @@ function initMap() {
           position: event.latLng,
           map: map,
         });
-      });
+    });
+
+    map.addListener('click', function(event) {
+        lat = event.latLng.lat();
+        lng = event.latLng.lng();
+        console.writeLine('Latitude: ' + lat + 'Longitude: ' + lng);
+    });
 }
