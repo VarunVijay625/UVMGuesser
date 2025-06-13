77e26d77c7220a280f0b32be28bc9a2083c0279b
Varun <vvijay@uvm.edu>
2025-06-12 20:18:53 -0400

    marker kind of working!

--- script.js
+++ script.js
@@ -7,17 +7,20 @@ function initMap() {
         mapTypeId: 'satellite'
     });
 
-    map.AdvancedMarkerElement({
-        position: { lat: 44.4781, lng: -73.1975 },
-        map : map
-    });
 
     map.addListener("click", function(event){
         var x = event.latLng.lat();
         var y = event.latLng.lng()
-        alert(x + " and also " + y);
+        //alert(x + " and also " + y);
         //marker.se
+        marker = new google.maps.Marker({
+            position: { lat: x, lng: y },
+            map:map,
+            label: "Guess"
+        })
+        //(marker.position)
     });
+
 
 
 
@@ -41,7 +44,3 @@ function initMap() {
     //  });
 }
 
-const marker = new AdvancedMarkerElement({
-        map,
-        position: { lat: 44.4781, lng: -73.1975 }
-});
