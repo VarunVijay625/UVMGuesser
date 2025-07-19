2e44f25112849a7701f16d66267d9fc737f5e30b
Varun <vvijay@uvm.edu>
2025-07-18 22:33:32 -0400

    now I am so close I can taste it

--- script.js
+++ script.js
@@ -16,14 +16,29 @@ let images = [
 'images/IMG_1518.jpg',
 ];
 
+locations_dictionary={ 
+    'images/IMG_8616.jpg':[44.47345500985082, -73.19680262565612],
+    'images/IMG_8680.jpg':[44.480008032615, -73.20030022621154],
+    'images/IMG_8681.jpg':[44.478121051856405, -73.19681871891021],
+    'images/IMG_8683.jpg':[44.47837941489673, -73.19493044376372],
+    'images/IMG_8684.jpg':[44.47818803498085, -73.19470245599746],
+    'images/IMG_8685.jpg':[44.47636989443445, -73.20160377979278],
+    'images/IMG_0212.jpg':[44.47830669060071, -73.20096273183822],
+    'images/IMG_9326.jpg':[44.47458231996633, -73.19646466732024],
+    'images/IMG_8832.jpg':[44.47648089832089, -73.19603014945983],
+    'images/IMG_2915.jpg':[44.47831434579355, -73.19455493450164],
+    'images/IMG_1095.jpg':[44.47492299684291, -73.19519330024718],
+    'images/IMG_0961.jpg':[44.47958318020233, -73.19629032373427],
+    'images/IMG_3466.jpg':[44.47146254600169, -73.1943671798706],
+    'images/IMG_1518.jpg':[44.47918703124267, -73.20018220901488],
+};
+
 const firstImage = 0;
 const lastImage = images.length -1;
 let currentImage = 0;
 
 function shuffle(array) {
   let currentIndex = array.length;
-
-
 
   while (currentIndex != 0) {
 
@@ -60,14 +75,32 @@ guessButton.addEventListener("click", doGuess)
 
 function doGuess(){
     var latlng = {
-        lat: marker.getPosition().lat(),
-        lng: marker.getPosition().lng()
+        latit: marker.getPosition().lat(),
+        long: marker.getPosition().lng()
     }
+    //console.log(latlng.latit + ", " + latlng.long + ", " + images[currentImage] + ", " + locations_dictionary[images[currentImage]][0])
+    // let lineCoords = [
+    //     {lat: latit, lng: long},
+    //     {lat: locations_dictionary[images[currentImage]][0], lng: locations_dictionary[images[currentImage]][1]}
+    // ]
+
     const imageTag = document.getElementById('image');
     currentImage++;
     imageTag.src = images[currentImage]
     //document.getElementById("round").innerHTML = latlng.lat + ", " + latlng.lng + ", " + images[currentImage];
-    console.log(latlng.lat + ", " + latlng.lng + ", " + images[currentImage])
+    var flightPlanCoordinates = [
+    { lat: latlng.latit, lng: latlng.long },
+    { lat: locations_dictionary[images[currentImage - 1]][0], lng: locations_dictionary[images[currentImage - 1]][1] }
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  flightPath.setMap(map);
 
 
     }
