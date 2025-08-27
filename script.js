dc902226dd1db3d5bc07d428a43225709ee5a390
Varun <vvijay@uvm.edu>
2025-08-26 20:37:50 -0400

    Dan's Edit

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ let images = [
 ];
 //get rid of the crow image
 locations_dictionary={ 
-    'images/IMG_8616.jpg':[44.47345500985082, -73.19680262565612],
-    'images/IMG_8680.jpg':[44.480008032615, -73.20030022621154],
-    'images/IMG_8681.jpg':[44.478121051856405, -73.19681871891021],
+    'images/IMG_8616.jpg':[44.47295163683276, -73.19708157539367],
+    'images/IMG_8680.jpg':[44.48004391527659, -73.20026669859885],
+    'images/IMG_8681.jpg':[44.47820277775, -73.19658611111112],
     'images/IMG_8683.jpg':[44.47837941489673, -73.19493044376372],
     'images/IMG_8684.jpg':[44.47818803498085, -73.19470245599746],
-    'images/IMG_8685.jpg':[44.47636989443445, -73.20160377979278],
-    'images/IMG_0212.jpg':[44.47830669060071, -73.20096273183822],
+    'images/IMG_8685.jpg':[44.47644979810942, -73.20142876565455],
+    'images/IMG_0212.jpg':[44.478310518195094, -73.20097614288329],
     'images/IMG_9326.jpg':[44.47458231996633, -73.19646466732024],
     'images/IMG_8832.jpg':[44.47648089832089, -73.19603014945983],
-    'images/IMG_2915.jpg':[44.47831434579355, -73.19455493450164],
-    'images/IMG_1095.jpg':[44.47492299684291, -73.19519330024718],
-    'images/IMG_0961.jpg':[44.47958318020233, -73.19629032373427],
-    'images/IMG_3466.jpg':[44.47146254600169, -73.1943671798706],
+    'images/IMG_2915.jpg':[44.478303819900596, -73.19456298112868],
+    'images/IMG_1095.jpg':[44.47888888888889, -73.20361111111112],
+    'images/IMG_0961.jpg':[44.479520026197996, -73.19630373477935],
+    'images/IMG_3466.jpg':[44.47144436278037, -73.1942961013317],
     'images/IMG_1518.jpg':[44.47918703124267, -73.20018220901488],
 };
 let guessed = true;
@@ -67,6 +67,10 @@ function initMap() {
             draggable: true
     })
 
+    marker.addListener('click', function(event){
+      console.log(marker.getPosition.lat(), marker.getPosition.lng());
+    })
+
 
 
   //currentImage++;
@@ -80,6 +84,7 @@ const guessButton = document.getElementById("nextBtn");
 guessButton.addEventListener("click", doGuess);
 
 function doGuess(){
+  console.log(document.getElementById("result"));
   if (guessed == false){
     document.getElementById("btnVal").innerHTML = "Next";
     guessed = true;
@@ -87,6 +92,7 @@ function doGuess(){
         latit: marker.getPosition().lat(),
         long: marker.getPosition().lng()
     }
+    console.log(latlng.latit, latlng.long, images[currentImage])
 
     var degN = latlng.latit - locations_dictionary[images[currentImage]][0];
     var degW = latlng.long - locations_dictionary[images[currentImage]][1];
@@ -96,12 +102,12 @@ function doGuess(){
     var exactResult = Math.sqrt(sqResult);
 
       
-    var flightPlanCoordinates = [
+    var guessLineCoordinates = [
       { lat: latlng.latit, lng: latlng.long },
       { lat: locations_dictionary[images[currentImage]][0], lng: locations_dictionary[images[currentImage]][1] }
     ];
-    const flightPath = new google.maps.Polyline({
-      path: flightPlanCoordinates,
+    const guessLine = new google.maps.Polyline({
+      path: guessLineCoordinates,
       geodesic: true,
       strokeColor: "#FF0000",
       strokeOpacity: 1.0,
@@ -109,7 +115,7 @@ function doGuess(){
     });
 
 
-    flightPath.setMap(map);
+    guessLine.setMap(map);
 
 
     var result = Math.round(exactResult);
@@ -149,7 +155,7 @@ function doGuess(){
     }
     totalPoints = totalPoints + points;
     
-    let exclamations = [
+    const exclamations = [
       "What were you thinking?",
       "Not even close!",
       "Too bad!",
@@ -163,12 +169,48 @@ function doGuess(){
       "Excellent!"
     ]
 
+
+
+
+
     document.getElementById("result").innerHTML = exclamations[points] + ' Your guess was \n' + result + '\n feet away (' + (exactResult / 5280).toFixed(2) + ' miles). You scored ' + points + '/10 points.';
     document.getElementById("points").innerHTML = totalPoints + '/100 points';
   }
   else {
     if (currentImage >= 10){
-    alert('ok');
+
+      //reset the button map page
+
+      // DAN COMMENTED THIS OUT
+      // document.getElementById("nextBtn").innerHTML = '<a href="finish.html"><p>Finish Game</p></a>';
+
+      document.getElementById("image").style.visibility = "hidden";
+      document.getElementById("map").style.visibility = "hidden";
+      document.getElementById("final").style.visibility = "visible";
+
+      const congrs = [
+        "You've got some room for improvement!",
+        "We all start somewhere!",
+        "Never give up!",
+        "It's only uphill from here!",
+        "You're on your way up!",
+        "Not half bad! Wait, nevermind. Exactly half bad.",
+        "That's mostly good, right?",
+        "That's pretty good!",
+        "You know your campus!",
+        "That's amazing!",
+        "WOW! You are a true UVMGuesser!!!!! This is amazingly impressive!!!!\n -Varun, the developer of the game, who can't even score this high",
+      ];
+
+      var finalPts = Math.floor(totalPoints/10)
+      console.log('hi');
+      console.log(document.getElementById("congrats"));
+      
+      document.getElementById("congrats").innerHTML = 'You scored a total of ' + totalPoints + 'points. ' + congrs[finalPts];
+
+      document.getElementById("stars").innerHTML = '<img src="images/0.png" alt="UVM"></img>';
+
+
     }
     initMap();
     document.getElementById("btnVal").innerHTML = "Guess";
